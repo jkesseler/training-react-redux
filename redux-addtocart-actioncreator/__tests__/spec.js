492d225b88429dd310a5f675ef033c0039653e64
Jorgen Kesseler <jkesseler@gmail.com>
2016-07-12 10:17:41 +0200

    Added action creator and test

--- redux-addtocart-actioncreator/__tests__/spec.js
+++ redux-addtocart-actioncreator/__tests__/spec.js
@@ -1,7 +1,21 @@
-import { addToCart } from '../index';
+import { addToCart, ADD_TO_CART } from '../index';
+import { isFSA } from 'flux-standard-action';
 
-describe('addToCart', () => {
+
+describe.only('addToCart', () => {
   it('should return an action object', () => {
-    // ...
+    const productNumber = '12345';
+    const amount = 13;
+    const result = addToCart(productNumber, amount);
+    const expected = {
+      type: ADD_TO_CART,
+      payload: {
+        productNumber,
+        amount
+      }
+    };
+
+    expect(isFSA(result)).to.be.equal(true);
+    expect(result).to.deep.equal(expected);
   });
 });
--- redux-addtocart-actioncreator/index.js
+++ redux-addtocart-actioncreator/index.js
@@ -1,1 +1,12 @@
-// export ...
+
+export const ADD_TO_CART = 'ADD_TO_CART';
+
+export function addToCart(productNumber, amount) {
+  return {
+    type: ADD_TO_CART,
+    payload: {
+      productNumber,
+      amount
+    }
+  };
+}
