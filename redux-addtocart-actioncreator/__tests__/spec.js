c04ca56bad97b03d2fac013b5beeceb1bf234853
Jorgen Kesseler <jkesseler@gmail.com>
2016-07-12 11:32:16 +0200

    addToCart reducer

--- redux-addtocart-actioncreator/__tests__/spec.js
+++ redux-addtocart-actioncreator/__tests__/spec.js
@@ -2,15 +2,15 @@ import { addToCart, ADD_TO_CART } from '../index';
 import { isFSA } from 'flux-standard-action';
 
 
-describe.only('addToCart', () => {
+describe('addToCart', () => {
   it('should return an action object', () => {
-    const productNumber = '12345';
+    const productId = 12345;
     const amount = 13;
-    const result = addToCart(productNumber, amount);
+    const result = addToCart(productId, amount);
     const expected = {
       type: ADD_TO_CART,
       payload: {
-        productNumber,
+        productId,
         amount
       }
     };
--- redux-addtocart-actioncreator/index.js
+++ redux-addtocart-actioncreator/index.js
@@ -1,11 +1,11 @@
 
 export const ADD_TO_CART = 'ADD_TO_CART';
 
-export function addToCart(productNumber, amount) {
+export function addToCart(productId, amount) {
   return {
     type: ADD_TO_CART,
     payload: {
-      productNumber,
+      productId,
       amount
     }
   };
